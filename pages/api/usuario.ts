dd40afb97411153ac69388351f9628f5a75118e0
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2022-09-23 13:17:36 -0300

    Get do usuario logado e feed pelo id do user

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,8 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+import {conectarMongoDB} from '../../middlewares/conectaMongoDB';
+import type {} from '../../types/RespostaPadraoMsg';
+import { UsuarioModel } from "../../models/UsuarioModel";
 
-const usuarioEndPoint = (req: NextApiRequest, res: NextApiResponse) => {
-    return res.status(200).json('Usuario autenticado com sucesso');
+const usuarioEndPoint = async (req: NextApiRequest, res: NextApiResponse | any) => {
+    try{
+    const{userId} = req?.query;
+    const usuario = await UsuarioModel.findById(userId);
+    usuario.senha = null;
+    return res.status(200).json(usuario);
+    }catch(e){
+        console.log(e);
+        return res.status(400).json({erro: 'Não foi possível obter dados do usuario'})
+    }
 }
 
-export default validarTokenJWT(usuarioEndPoint);
+export default validarTokenJWT(conectarMongoDB(usuarioEndPoint));
