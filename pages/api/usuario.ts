71d1b10635ebbdbcb0f1a76007d09f0870edd74f
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2022-09-23 16:53:54 -0300

    Configuração do handler de put e get do usuário

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,19 +1,60 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import {conectarMongoDB} from '../../middlewares/conectaMongoDB';
-import type {} from '../../types/RespostaPadraoMsg';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import { UsuarioModel } from "../../models/UsuarioModel";
+import nc from 'next-connect';
+import {upload,uploadImagemCosmic} from '../../services/uploadImagemCosmic';
 
-const usuarioEndPoint = async (req: NextApiRequest, res: NextApiResponse | any) => {
-    try{
-    const{userId} = req?.query;
-    const usuario = await UsuarioModel.findById(userId);
-    usuario.senha = null;
-    return res.status(200).json(usuario);
-    }catch(e){
-        console.log(e);
-        return res.status(400).json({erro: 'Não foi possível obter dados do usuario'})
+
+const handler = nc()
+    .use(upload.single('file'))
+    .put(
+        async(req : any, res: NextApiResponse<RespostaPadraoMsg>) =>{
+            try{
+                const {userId} = req?.query;
+                const usuario = await UsuarioModel.findById(userId);
+
+                if(!usuario){
+                    return res.status(400).json({erro: 'Usuário não encontrado'});
+                }
+
+                const {nome} = req.body;
+                if(nome && nome.length >2){
+                    usuario.nome = nome;                
+                }
+                
+                const {file} = req;
+                if(file && file.originalname){
+                    const image = await uploadImagemCosmic(req);
+                    if(image && image.media && image.media.url){
+                        usuario.avatar = image.media.url;
+                    }
+                }
+
+                await UsuarioModel.findByIdAndUpdate({_id: usuario._id}, usuario);
+                return res.status(200).json({msg: 'Usuario alterado com sucesso'})
+            }catch(e){
+                console.log(e);
+                return res.status(400).json({erro: 'Não foi possível atualizar o usuário'})
+            }
+    })
+    .get(async (req: NextApiRequest, res: NextApiResponse | any) => {
+        try{
+        const{userId} = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+        }catch(e){
+            console.log(e);
+            return res.status(400).json({erro: 'Não foi possível obter dados do usuario', e})
+        }
+    });
+
+export const config ={
+    api: {
+        bodyParser: false
     }
 }
 
-export default validarTokenJWT(conectarMongoDB(usuarioEndPoint));
+export default validarTokenJWT(conectarMongoDB(handler));
